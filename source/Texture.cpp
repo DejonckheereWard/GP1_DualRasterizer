9bc549db43669a4efb51b59a4cafc1d2eceadb77
Dejonckheere Ward <ward.dejonckheere@student.howest.be>
2023-01-20 17:25:09 +0100

    Adjusted Texture class for Software and Hardware rasterizing

--- source/Texture.cpp
+++ source/Texture.cpp
@@ -12,6 +12,10 @@ Texture::~Texture()
 {
 	m_pShaderResourceView->Release();
 	m_pResource->Release();
+
+	SDL_FreeSurface(m_pSurface);
+	//m_pSurface = nullptr;
+
 }
 
 
@@ -22,15 +26,83 @@ Texture* Texture::LoadFromFile(ID3D11Device* pDevice, const std::string& path)
 	//Load SDL_Surface using IMG_LOAD
 	//Create & Return a new Texture Object (using SDL_Surface)
 	SDL_Surface* pSurface = IMG_Load(path.c_str());
-	assert(pSurface != nullptr);
-	Texture* pTexture = new Texture(pDevice, pSurface);
 
-	// Cleanup
-	SDL_FreeSurface(pSurface);
-	return pTexture;
+	assert(pSurface != nullptr);
+	
+	return new Texture(pDevice, pSurface);
 }
 
-Texture::Texture(ID3D11Device* pDevice, SDL_Surface* pSurface)
+ColorRGB Texture::Sample(const Vector2& uv, UVMode uvMode) const
+{
+
+	int x = 0;
+	int y = 0;
+
+	switch(uvMode)
+	{
+
+		case UVMode::Wrap:
+		{
+			float uvX = uv.x;
+			float uvY = uv.y;
+
+			if(uvX < 0)
+				uvX += abs(int(uvX)) + 1;
+			if(uvY < 0)
+				uvY += abs(int(uvY)) + 1;
+
+			x = int(uvX * m_pSurface->w) % m_pSurface->w;
+			y = int(uvY * m_pSurface->h) % m_pSurface->h;
+			break;
+		}
+
+		case UVMode::Clamp:
+			x = int(uv.x * m_pSurface->w);
+			y = int(uv.y * m_pSurface->h);
+			x = Clamp(x, 0, m_pSurface->w);
+			y = Clamp(y, 0, m_pSurface->h);
+			break;
+
+		case UVMode::Mirror:
+			x = int(uv.x * m_pSurface->w);
+			y = int(uv.y * m_pSurface->h);
+			if(x % 2 == 0)
+				x = x % m_pSurface->w;
+			else
+				x = m_pSurface->w - (x % m_pSurface->w);
+			break;
+
+		case UVMode::Border:
+			x = int(uv.x * m_pSurface->w);
+			y = int(uv.y * m_pSurface->h);
+			if(x < 0 || x >= m_pSurface->w || y < 0 || y >= m_pSurface->h)
+				return ColorRGB{ 1.0f,0,1.0f };
+			break;
+
+
+		default:
+			assert(false && "Shouldn't ever hit this in the switch");
+			break;
+
+	}
+
+	// pixel color is in 0-255 ranges  0xFF FF FF FF -> ALPHA, BLUE, GREEN, RED
+	const uint32_t pixelColor = m_pSurfacePixels[(y * m_pSurface->w) + x];
+
+	ColorRGB color{};
+	color.r = float((pixelColor >> 0) & 0xFF);  // 0 shift because RED is least significnat, 0xFF because we want to mask out the other colors (only take last byte)
+	color.g = float((pixelColor >> 8) & 0xFF);
+	color.b = float((pixelColor >> 16) & 0xFF);
+	//color.a = (pixelColor >> 24) & 0xFF; // No A component apparently 
+
+	color /= 255.0f;  // "Normalize" from 0->1
+
+	return color;
+}
+
+Texture::Texture(ID3D11Device* pDevice, SDL_Surface* pSurface):
+	m_pSurface{pSurface},
+	m_pSurfacePixels{ (uint32_t*)pSurface->pixels}
 {
 
 	// Assemble the resource and shader resource view for directx
--- source/Texture.h
+++ source/Texture.h
@@ -21,11 +21,17 @@ public:
 
 	static Texture* LoadFromFile(ID3D11Device* pDevice, const std::string& path);
 	ID3D11ShaderResourceView* GetShaderResourceView() const { return m_pShaderResourceView; };
-
+	
+	ColorRGB Sample(const Vector2& uv, UVMode uvMode = UVMode::Wrap) const;
 
 private:
 	Texture(ID3D11Device* pDevice, SDL_Surface* pSurface);
 
+	// Software
+	SDL_Surface* m_pSurface{ nullptr };
+	uint32_t* m_pSurfacePixels{ nullptr };
+
+	// DirectX
 	ID3D11Texture2D* m_pResource{};
 	ID3D11ShaderResourceView* m_pShaderResourceView{};
 };
